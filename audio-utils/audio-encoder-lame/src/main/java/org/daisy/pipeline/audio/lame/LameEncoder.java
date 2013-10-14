be885b17bb8d8ab221ed93b45071aa191c63ed0f
Romain Mouret <rom1mouret@gmail.com>
2013-10-14 14:12:39 +0200

    take lame's path from system.properties or from the PATH

--- audio-utils/audio-encoder-lame/src/main/java/org/daisy/pipeline/audio/lame/LameEncoder.java
+++ audio-utils/audio-encoder-lame/src/main/java/org/daisy/pipeline/audio/lame/LameEncoder.java
@@ -10,80 +10,91 @@ import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+
 import org.daisy.pipeline.audio.AudioEncoder;
 
 public class LameEncoder implements AudioEncoder {
-    private static final String InputFormat = ".wav";
-    private static final String OutputFormat = ".mp3";
-    private String mLamePath;
-    
-    private static String findExecutableOnPath(String executableName){  
-        String systemPath = System.getenv("PATH");  
-        String[] pathDirs = systemPath.split(File.pathSeparator);  
-   
-        File fullyQualifiedExecutable = null;  
-        for (String pathDir : pathDirs)  {  
-            File file = new File(pathDir, executableName);  
-            if (file.isFile()){  
-                fullyQualifiedExecutable = file;  
-                break;  
-            }  
-        }  
-        return fullyQualifiedExecutable.getAbsolutePath();  
-    }  
-    
+	private static final String InputFormat = ".wav";
+	private static final String OutputFormat = ".mp3";
+	private String mLamePath;
 
-    //TODO: take the path from the properties if it exists
-    //and raise an exception if nothing is found
-    public LameEncoder() {
-    	mLamePath = findExecutableOnPath("lame");
-    }
+	private String findBinary(String propertyName, String executableName) {
+		String result = System.getProperty(propertyName);
+		if (result != null)
+			return result;
 
-    @Override
-    public String encode(byte[] input, int size, AudioFormat audioFormat,
-            Object caller, String name) {
-        // generate the intermediate WAV file from the input audio data
-        File intermediateFile;
-        try {
-            intermediateFile = File.createTempFile("chunk", InputFormat);
-        } catch (IOException e1) {
-            return null;
-        }
-        try {
-            InputStream inputStream = new ByteArrayInputStream(input);
-            AudioInputStream ais = new AudioInputStream(inputStream,
-                    audioFormat, size / audioFormat.getFrameSize());
+		String systemPath = System.getenv("PATH");
+		String[] pathDirs = systemPath.split(File.pathSeparator);
 
-            AudioSystem.write(ais, AudioFileFormat.Type.WAVE, intermediateFile);
-            ais.close();
-            inputStream.close();
-        } catch (FileNotFoundException e1) {
-            intermediateFile.delete();
-            return null;
-        } catch (IOException e) {
-            intermediateFile.delete();
-            return null;
-        }
+		for (String pathDir : pathDirs) {
+			File file = new File(pathDir, executableName);
+			if (file.isFile()) {
+				return file.getAbsolutePath();
+			}
+		}
+		return null;
+	}
 
-        // execute the lame binary on the intermediate WAV file
-        File encodedFile = null;
-        String[] cmd = null;
-        try {
-            encodedFile = File.createTempFile("chunk", OutputFormat);
-            cmd = new String[] {
-                    mLamePath, "--silent",
-                    intermediateFile.getAbsolutePath(),
-                    encodedFile.getAbsolutePath()
-            };
-            Runtime.getRuntime().exec(cmd).waitFor();
+	//TODO: call the TTS's BinaryFinder.find instead of findBinary
+	public LameEncoder() throws InterruptedException, IOException {
+		final String property = "lame.path";
+		mLamePath = findBinary(property, "lame");
+		if (mLamePath == null) {
+			throw new RuntimeException("Cannot find lame in PATH and "
+			        + property + " is not set");
+		}
 
-        } catch (Exception e) {
-            encodedFile = null;
-            return null;
-        } finally {
-            intermediateFile.delete();
-        }
+		//check that the encoder can be run
+		String[] cmd = new String[]{
+		        mLamePath, "--help"
+		};
+		Runtime.getRuntime().exec(cmd).waitFor();
+	}
 
-        return encodedFile.toURI().toString();
-    }
+	@Override
+	public String encode(byte[] input, int size, AudioFormat audioFormat,
+	        Object caller, String name) {
+		// generate the intermediate WAV file from the input audio data
+		File intermediateFile;
+		try {
+			intermediateFile = File.createTempFile("chunk", InputFormat);
+		} catch (IOException e1) {
+			return null;
+		}
+		try {
+			InputStream inputStream = new ByteArrayInputStream(input);
+			AudioInputStream ais = new AudioInputStream(inputStream,
+			        audioFormat, size / audioFormat.getFrameSize());
+
+			AudioSystem.write(ais, AudioFileFormat.Type.WAVE, intermediateFile);
+			ais.close();
+			inputStream.close();
+		} catch (FileNotFoundException e1) {
+			intermediateFile.delete();
+			return null;
+		} catch (IOException e) {
+			intermediateFile.delete();
+			return null;
+		}
+
+		// execute the lame binary on the intermediate WAV file
+		File encodedFile = null;
+		String[] cmd = null;
+		try {
+			encodedFile = File.createTempFile("chunk", OutputFormat);
+			cmd = new String[]{
+			        mLamePath, "--silent", intermediateFile.getAbsolutePath(),
+			        encodedFile.getAbsolutePath()
+			};
+			Runtime.getRuntime().exec(cmd).waitFor();
+
+		} catch (Exception e) {
+			encodedFile = null;
+			return null;
+		} finally {
+			intermediateFile.delete();
+		}
+
+		return encodedFile.toURI().toString();
+	}
 }
