6d548dd5ff7454a3938f208c3ba1ef204f875570
Romain Mouret <rom1mouret@gmail.com>
2014-05-15 12:29:56 +0200

    - Dynamic policy for the registry; - Updated the pipeline-framework's modules versions; - Made possible to return a null audio encoder.

--- audio-utils/audio-common/src/main/java/org/daisy/pipeline/audio/AudioServices.java
+++ audio-utils/audio-common/src/main/java/org/daisy/pipeline/audio/AudioServices.java
@@ -1,24 +1,26 @@
 package org.daisy.pipeline.audio;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
-
-import org.daisy.pipeline.audio.AudioEncoder;
 
 public class AudioServices {
 
-    private final Collection<AudioEncoder> encoders = new CopyOnWriteArrayList<AudioEncoder>();
+	private final Collection<AudioEncoder> encoders = new CopyOnWriteArrayList<AudioEncoder>();
 
-    public AudioEncoder getEncoder() {
-        return encoders.iterator().next();
-    }
+	public AudioEncoder getEncoder() {
+		Iterator<AudioEncoder> it = encoders.iterator();
+		if (it.hasNext())
+			return it.next();
+		return null;
+	}
 
-    public void addEncoder(AudioEncoder encoder) {
-        encoders.add(encoder);
-    }
+	protected void addEncoder(AudioEncoder encoder) {
+		encoders.add(encoder);
+	}
 
-    public void removeEncoder(AudioEncoder encoder) {
-        encoders.remove(encoder);
-    }
+	protected void removeEncoder(AudioEncoder encoder) {
+		encoders.remove(encoder);
+	}
 
 }
