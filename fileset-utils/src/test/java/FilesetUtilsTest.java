68116a67000d62d7c8c1b608ec532f610a513b2d
Bert Frees <bertfrees@gmail.com>
2014-12-02 15:49:19 +0100

    px:fileset-store: support !/ in target HREFs of file sets
    see issue #70

---
+++ fileset-utils/src/test/java/FilesetUtilsTest.java
@@ -0,0 +1,69 @@
+import java.io.File;
+
+import javax.inject.Inject;
+
+import org.daisy.maven.xproc.xprocspec.XProcSpecRunner;
+
+import static org.daisy.pipeline.pax.exam.Options.calabashConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
+import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
+import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.pipelineModule;
+import static org.daisy.pipeline.pax.exam.Options.thisBundle;
+import static org.daisy.pipeline.pax.exam.Options.xprocspecBundles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.util.PathUtils;
+
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class FilesetUtilsTest {
+	
+	@Inject
+	private XProcSpecRunner runner;
+	
+	@Configuration
+	public Option[] config() {
+		return options(
+			logbackConfigFile(),
+			calabashConfigFile(),
+			logbackBundles(),
+			felixDeclarativeServices(),
+			pipelineModule("file-utils"),
+			pipelineModule("common-utils"),
+			pipelineModule("html-utils"),
+			pipelineModule("zip-utils"),
+			pipelineModule("mediatype-utils"),
+			xprocspecBundles(),
+			thisBundle(),
+			junitBundles()
+		);
+	}
+	
+	@Inject
+	private XProcSpecRunner xprocspecRunner;
+		
+	@Test
+	public void runXProcSpec() throws Exception {
+		File baseDir = new File(PathUtils.getBaseDir());
+		boolean success = xprocspecRunner.run(new File(baseDir, "src/test/xprocspec"),
+		                                      new File(baseDir, "target/xprocspec-reports"),
+		                                      new File(baseDir, "target/surefire-reports"),
+		                                      new File(baseDir, "target/xprocspec"),
+		                                      new XProcSpecRunner.Reporter.DefaultReporter());
+		assertTrue("XProcSpec tests should run with success", success);
+	}
+}
