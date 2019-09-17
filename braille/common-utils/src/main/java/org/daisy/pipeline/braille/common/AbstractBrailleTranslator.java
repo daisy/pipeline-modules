daisy/pipeline-mod-braille

BrailleTranslator.hasNext() should return false if getTranslatedRemainder is empty
this fixes commit 3d836fe

common-utils/src/main/java/org/daisy/pipeline/braille/common/AbstractBrailleTranslator.java
@@ -121,10 +121,12 @@ public abstract class AbstractBrailleTranslator extends AbstractTransform implem
 			protected static class FullyHyphenatedAndTranslatedString implements BrailleStream {
 				private String next;
 				public FullyHyphenatedAndTranslatedString(String string) {
+					if (string.replaceAll("[\u00ad\u200b]","").isEmpty())
+						string = null;
 					next = string;
 				}
 				public boolean hasNext() {
-					return (next != null && !next.isEmpty());
+					return next != null;
 				}
 				public String next(int limit, boolean force, boolean allowHyphens) {
 					if (next == null)

daisy/pipeline-modules

braille/common-utils/src/main/java/org/daisy/pipeline/braille/common/AbstractBrailleTranslator.java
@@ -186,7 +186,8 @@ public abstract class AbstractBrailleTranslator extends AbstractTransform implem
 						} else
 							next = next.substring(0, to);
 					}
-					if (next.length() == 0) next = null;
+					if (next.replaceAll("[\u00ad\u200b]","").isEmpty())
+						next = null;
 					alwaysEmpty = (next == null);
 					this.hyphenChar = hyphenChar;
 				}