ecf0dc05bce7f307829960435578d9567624f0d7
Louis <louis.caille@hotmail.fr>
2020-07-06 16:22:36 +0200

    Request Scheduler - add a waiting time
    I handle exceptions "too many requests" in addition to counting the number of requests and the number of characters

--- src/main/java/org/daisy/pipeline/tts/google/impl/RequestScheduler.java
+++ src/main/java/org/daisy/pipeline/tts/google/impl/RequestScheduler.java
@@ -1,25 +1,11 @@
 package org.daisy.pipeline.tts.google.impl;
 
-import java.util.UUID;
-
 public interface RequestScheduler {
+	
+	void addWaitingTime(int time);
+	
+	void sleep() throws InterruptedException;
 
-	static class TRequest {
-		
-		public int nbChar;
-		public int maxRequestsByMinute;
-		public int maxCharByMinute;
-		
-		public TRequest(int nbChar, int maxRequestsByMinute, int maxCharByMinute) {
-			this.nbChar = nbChar;
-			this.maxRequestsByMinute = maxRequestsByMinute;
-			this.maxCharByMinute = maxCharByMinute;
-		}
-
-	}
-
-	UUID addRequest(TRequest req);
-
-	TRequest getRequest(UUID requestID) throws InterruptedException;
+	void addRequest(int nbChar, int waitingTime) throws InterruptedException;
 
 }
