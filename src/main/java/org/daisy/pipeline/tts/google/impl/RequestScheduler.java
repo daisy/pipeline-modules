f39469dcf1786eaf9a9100e624477954b5d4f9d8
Louis <louis.caille@hotmail.fr>
2020-07-10 11:30:56 +0200

    RequestScheduler - only keep the sleep method

--- src/main/java/org/daisy/pipeline/tts/google/impl/RequestScheduler.java
+++ src/main/java/org/daisy/pipeline/tts/google/impl/RequestScheduler.java
@@ -1,36 +1,12 @@
 package org.daisy.pipeline.tts.google.impl;
 
 public interface RequestScheduler {
-	
+
 	/**
 	 * 
-	 * @param the waiting time that we want to add to the variable waitingTime.
-	 * 
-	 */
-	void addWaitingTime(int time);
-	
-	/**
-	 * 
-	 * sleep the thread according to the waiting time.
+	 * sleep the thread based on exponential backoff.
 	 * 
 	 */
 	void sleep() throws InterruptedException;
-	
-	/**
-	 * 
-	 * @param the number of characters in the sentence to be synthesized and the waiting time.
-	 * 
-	 */
-	void addRequest(int nbChar, int waitingTime) throws InterruptedException;
-	
-	
-	/**
-	 * 
-	 * @param the number of characters in the sentence to be synthesized and the waiting time.
-	 * 
-	 * checks if the quota on characters will not be exceeded
-	 * 
-	 */
-	void assertChar(int nbChar, int waitingTime) throws InterruptedException;
 
 }
