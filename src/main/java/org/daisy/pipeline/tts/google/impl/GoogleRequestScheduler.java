8056131f795e9a339f79cfe4e8259144660e7164
Louis <louis.caille@hotmail.fr>
2020-07-10 11:31:19 +0200

    GoogleRequestScheduler - sleep method based on exponential backoff

--- src/main/java/org/daisy/pipeline/tts/google/impl/GoogleRequestScheduler.java
+++ src/main/java/org/daisy/pipeline/tts/google/impl/GoogleRequestScheduler.java
@@ -1,64 +1,20 @@
 package org.daisy.pipeline.tts.google.impl;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 public class GoogleRequestScheduler implements RequestScheduler {
 	
-	private int waitingTime = 0;
-	private int nbRequests = 0;
-	private int nbChar = 0;
-	private LocalDateTime timestamp;
-
-	// 65000ms is used as a wait time with the Google API
-	// quotas are 300 requests per minute or 15000 characters
+	private int waitingTime;
+	private int random_number_milliseconds;
+	private int n = 0;
 	
-	@Override
-	public synchronized void addWaitingTime(int time) {
-		waitingTime += time;
-	}
-
-	@Override
+	// maximum waiting time in millisecond
+	private static final int MAXIMUM_BACKOFF = 64000;
+	
+	// https://cloud.google.com/storage/docs/exponential-backoff
 	public synchronized void sleep() throws InterruptedException {
-		if (waitingTime > 0) {
-			Thread.sleep(waitingTime);
-			// after a sleep everything is reset to zero
-			waitingTime = 0;
-			nbRequests = 0;
-			this.nbChar = 0;
-		}
-	}
-	
-	@Override
-	public synchronized void addRequest(int nbChar, int time) throws InterruptedException {
-		
-		if (timestamp == null) {
-			timestamp = LocalDateTime.now();
-		}
-		
-		// if more than a minute has passed since the last request then the counters are set to 0
-		if ( Duration.between(timestamp, LocalDateTime.now()).getSeconds() > 60 ) {		
-			nbRequests = 0;
-			this.nbChar = 0;
-			timestamp = LocalDateTime.now();	
-		}
-		
-		nbRequests++;
-		this.nbChar += nbChar;
-		
-		// if quotas are exceeded, waiting time is added
-		if ( Duration.between(timestamp, LocalDateTime.now()).getSeconds() <= 60 
-				&& (nbRequests >= 300 || this.nbChar >= 15000) ) {	
-			addWaitingTime(time);	
-		}
-	}
-	
-	@Override
-	public synchronized void assertChar(int nbChar, int time) throws InterruptedException {
-		if (this.nbChar + nbChar > 15000) {
-			addWaitingTime(time);
-			sleep();
-		}
+		random_number_milliseconds = (int) (Math.random() * 1000);
+		waitingTime = (int) Math.min(Math.pow(2, n) + random_number_milliseconds, MAXIMUM_BACKOFF);
+		Thread.sleep(waitingTime);
+		n++;
 	}
 	
 
