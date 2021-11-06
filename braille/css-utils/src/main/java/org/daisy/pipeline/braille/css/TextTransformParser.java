6ef8819545c34ff668a5ad3ee06f7d8425d35138
Bert Frees <bertfrees@gmail.com>
2021-11-06 19:11:25 +0100

    Handle more "system" values in @text-transform rules
    - "braille" is the same as "braille-translator"
    - "liblouis" selects only Liblouis translators
    - a value that matches a Liblouis "system" metadata field is also
      supported
    - the default value is "braille" (so @text-transform rules without a
      "system" descriptor select braille translators too)

--- braille/css-utils/src/main/java/org/daisy/pipeline/braille/css/TextTransformParser.java
+++ braille/css-utils/src/main/java/org/daisy/pipeline/braille/css/TextTransformParser.java
@@ -29,62 +29,82 @@ public class TextTransformParser {
 	public static Map<String,Query> getBrailleTranslatorQueries(String style, URI baseURI, Query baseQuery) {
 		if (style != null && !"".equals(style)) {
 			Map<String,Query> queries = null;
-			for (RuleTextTransform rule : filter(new InlineStyle(style), RuleTextTransform.class))
+			for (RuleTextTransform rule : filter(new InlineStyle(style), RuleTextTransform.class)) {
+				// default system is braille
+				String system = "braille";
 				for (Declaration d : rule)
-					if (d.getProperty().equals("system")
-					    && d.size() == 1
-					    && d.get(0) instanceof TermIdent
-					    && "braille-translator".equals(((TermIdent)d.get(0)).getValue())) {
-						MutableQuery query = mutableQuery(baseQuery);
-						for (Declaration dd : rule)
-							if (dd.getProperty().equals("system")
-							    && dd.size() == 1
-							    && dd.get(0) instanceof TermIdent
-							    && "braille-translator".equals(((TermIdent)dd.get(0)).getValue()))
-								;
-							else if (!dd.getProperty().equals("system")
-							         && dd.size() == 1
-							         && (dd.get(0) instanceof TermIdent
-							             || dd.get(0) instanceof TermURI
-							             || dd.get(0) instanceof TermInteger)) {
-								String key = dd.getProperty();
-								String value;
-								if (dd.get(0) instanceof TermURI) {
-									URL cssBase = ((TermURI)dd.get(0)).getBase(); // this is always null because the source is a string
-									value = URLs.resolve(cssBase != null ? URLs.asURI(cssBase) : baseURI,
-									                     URLs.asURI(((TermURI)dd.get(0)).getValue()))
-										.toASCIIString();
-								} else {
-									if (dd.get(0) instanceof TermInteger)
-										value = "" + ((TermInteger)dd.get(0)).getIntValue();
-									else
-										value = "" + dd.get(0).getValue();
-									if (query.containsKey(key))
-										query.removeAll(key);
-								}
-								if (key.equals("contraction") && value.equals("no"))
-									query.removeAll("grade");
-								// FIXME: support this in Liblouis translator
-								if (key.equals("table") || key.equals("liblouis-table")) {
-									query.removeAll("locale");
-									query.removeAll("type");
-									query.removeAll("contraction");
-									query.removeAll("grade");
-									query.removeAll("dots");
-									query.removeAll("direction");
-								}
-								query.add(key, value);
-							} else {
-								query = null;
-								break;
-							}
-						if (query != null) {
-							if (queries == null) queries = new HashMap<>();
-							String name = rule.getName();
-							queries.put(name == null ? "auto" : name, query);
+					if (d.getProperty().equals("system")) {
+						if (d.size() == 1
+						    && d.get(0) instanceof TermIdent)
+							system = ((TermIdent)d.get(0)).getValue();
+						else
+							system = null;
+						break; }
+				if (system == null)
+					continue;
+				if ("braille-translator".equals(system))
+					system = "braille";
+				String translator = null;
+				if ("liblouis".equals(system)) {
+					system = "braille";
+					translator = "liblouis"; }
+				MutableQuery query = mutableQuery(baseQuery);
+				// System could be "braille" or something else. Because at this point DAISY Pipeline
+				// does not support any other systems than braille systems, we assume that
+				// "something else" could be a feature that Liblouis or another translator provider
+				// may support.
+				if (!"braille".equals(system)) {
+					if (query.containsKey("system"))
+						query.removeAll("system");
+					query.add("system", system); }
+				if (translator != null)
+					query.add("translator", translator);
+				for (Declaration d : rule)
+					if (d.getProperty().equals("system")) continue;
+					else if (d.size() == 1
+					         && (d.get(0) instanceof TermIdent
+					             || d.get(0) instanceof TermURI
+					             || d.get(0) instanceof TermInteger)) {
+						String key = d.getProperty();
+						String value;
+						if (d.get(0) instanceof TermURI) {
+							URL cssBase = ((TermURI)d.get(0)).getBase(); // this is always null because the source is a string
+							value = URLs.resolve(cssBase != null ? URLs.asURI(cssBase) : baseURI,
+							                     URLs.asURI(((TermURI)d.get(0)).getValue()))
+							            .toASCIIString();
+						} else {
+							if (d.get(0) instanceof TermInteger)
+								value = "" + ((TermInteger)d.get(0)).getIntValue();
+							else
+								value = "" + d.get(0).getValue();
+							if (query.containsKey(key))
+								query.removeAll(key);
 						}
+						if (translator != null && key.equals("translator") && !translator.equals(value)) {
+							query = null;
+							break; }
+						else if (key.equals("contraction") && value.equals("no"))
+							query.removeAll("grade");
+						// FIXME: support this in Liblouis translator
+						else if (key.equals("table") || key.equals("liblouis-table")) {
+							query.removeAll("locale");
+							query.removeAll("type");
+							query.removeAll("contraction");
+							query.removeAll("grade");
+							query.removeAll("dots");
+							query.removeAll("direction");
+						}
+						query.add(key, value);
+					} else {
+						query = null;
 						break;
 					}
+				if (query != null) {
+					if (queries == null) queries = new HashMap<>();
+					String name = rule.getName();
+					queries.put(name == null ? "auto" : name, query);
+				}
+			}
 			return queries;
 		} else
 			return null;
