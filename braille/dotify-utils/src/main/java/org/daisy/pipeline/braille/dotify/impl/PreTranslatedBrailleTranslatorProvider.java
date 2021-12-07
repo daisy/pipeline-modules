070d325545b8acef82e6be8b4b20cdeb2ac01e8d
Bert Frees <bertfrees@gmail.com>
2021-12-07 10:16:45 +0100

    Get rid of NumberBrailleTranslator
    because it is only used in tests (and we have another version of
    NumberBrailleTranslator dedicated for the tests).

--- braille/dotify-utils/src/main/java/org/daisy/pipeline/braille/dotify/impl/PreTranslatedBrailleTranslatorProvider.java
+++ braille/dotify-utils/src/main/java/org/daisy/pipeline/braille/dotify/impl/PreTranslatedBrailleTranslatorProvider.java
@@ -9,11 +9,12 @@ import org.daisy.pipeline.braille.common.Query;
 import org.daisy.pipeline.braille.common.Query.MutableQuery;
 import static org.daisy.pipeline.braille.common.Query.util.mutableQuery;
 import org.daisy.pipeline.braille.common.UnityBrailleTranslator;
+import static org.daisy.pipeline.braille.common.util.Locales.parseLocale;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * {@link BrailleTranslatorProvider} of PreTranslatedBrailleTranslator and NumberBrailleTranslator.
+ * {@link BrailleTranslatorProvider} of PreTranslatedBrailleTranslator.
  */
 @Component(
 	name = "org.daisy.pipeline.braille.dotify.impl.PreTranslatedBrailleTranslatorProvider",
@@ -26,28 +27,30 @@ public class PreTranslatedBrailleTranslatorProvider extends AbstractTransformPro
 
 	protected Iterable<BrailleTranslator> _get(Query query) {
 		MutableQuery q = mutableQuery(query);
-		q.removeAll("document-locale");
 		boolean isPreTranslatedQuery = false; {
-			for (Query.Feature f : q)
-				if ("input".equals(f.getKey()) && "braille".equals(f.getValue().orElse(null)))
-					isPreTranslatedQuery = true;
-				else if (!("input".equals(f.getKey()) && "text-css".equals(f.getValue().orElse(null)) ||
-				           "output".equals(f.getKey()) && "braille".equals(f.getValue().orElse(null)))) {
+			for (Query.Feature f : q) {
+				String key = f.getKey();
+				String val = f.getValue().orElse(null);
+				if ("input".equals(key)) {
+					if ("braille".equals(val))
+						isPreTranslatedQuery = true;
+					else if (!"text-css".equals(val)) {
+						isPreTranslatedQuery = false;
+						break; }}
+				else if ("output".equals(key)) {
+					if (!"braille".equals(val)) {
+						isPreTranslatedQuery = false;
+						break; }}
+				else if ("document-locale".equals(key) && val != null && !isPreTranslatedQuery) {
+					try {
+						if ("Brai".equals(parseLocale(val).getScript()))
+							isPreTranslatedQuery = true; }
+					catch (IllegalArgumentException e) {}}
+				else {
 					isPreTranslatedQuery = false;
-					break; }}
+					break; }}}
 		if (isPreTranslatedQuery)
 			return AbstractTransformProvider.util.Iterables.of(new UnityBrailleTranslator(null));
-		try {
-			for (Query.Feature f : q.removeAll("input"))
-				if (!"text-css".equals(f.getValue().get()))
-					throw new NoSuchElementException();
-			for (Query.Feature f : q.removeAll("output"))
-				if (!"braille".equals(f.getValue().get()))
-					throw new NoSuchElementException();
-			if (!q.isEmpty())
-				throw new NoSuchElementException();
-			return AbstractTransformProvider.util.Iterables.of(NumberBrailleTranslator.getInstance());
-		} catch (NoSuchElementException e) {}
 		return AbstractTransformProvider.util.Iterables.<BrailleTranslator>empty();
 	}
 }
